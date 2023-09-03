0d45f01710f34481ea92246e85c8069ceaaa6bc1
MattPChoy <matthewpchoy@gmail.com>
2023-09-03 21:56:43 +1000

    Modify to use postgresql

--- test/5_Databases/processor/processor.py
+++ test/5_Databases/processor/processor.py
@@ -3,7 +3,20 @@ import pika
 import json
 import sqlite3
 
-class Microservice():
+from sqlalchemy import create_engine
+
+db_name = 'database'
+db_user = 'username'
+db_pass = 'secret'
+db_host = 'db'
+db_port = '5432'
+
+# Connect to the database
+db_string = 'postgresql://{}:{}@{}:{}/{}'.format(db_user, db_pass, db_host, db_port, db_name)
+db = create_engine(db_string)
+
+
+class Microservice:
     def __init__(self, AMQP_URL=None):
         self.AMQP_URL = os.environ.get("AMQP_URL", "amqp://guest:guest@localhost/%2f") if AMQP_URL is None else AMQP_URL
         self.url_params = pika.URLParameters(self.AMQP_URL)
@@ -18,28 +31,33 @@ class Microservice():
             self.read_queues[queue_name] = ch
             ch.queue_declare(queue=queue_name, durable=durable)
             ch.basic_qos(prefetch_count=1)
+        else:
+            ch = self.read_queues[queue_name]
         ch.basic_consume(queue=queue_name, on_message_callback=callback)
+
     def run(self):
         def handle_bms_input(ch, method, properties, body):
             in_json = json.loads(body.decode('utf-8'))
             print(in_json)
             if in_json["event_type"] == "ADD_BATTERY":
-                self.db.execute(
-                    """INSERT INTO batteries(capacity, owner_id, name) VALUES(?, ?, ?)""",
-                    (in_json["capacity"], in_json["owner_id"], in_json["name"])
+                # self.db.execute(
+                #     """INSERT INTO batteries(capacity, owner_id, name) VALUES(?, ?, ?)""",
+                #     (in_json["capacity"], in_json["owner_id"], in_json["name"])
+                # )
+                # self.db.commit()
+                db.execute("INSERT INTO batteries(capacity, owner_id, name) VALUES(%s, %s, %d)").format(
+                    in_json["capacity"], in_json["owner_id"], in_json["name"]
                 )
-                self.db.commit()
-            if in_json["event_type"] == "DUMP":
-            	res = self.db.execute("SELECT * FROM batteries")
-            	print(res.fetchall())
+            # if in_json["event_type"] == "DUMP":
+            #     res = self.db.execute("SELECT * FROM batteries")
+            #     print(res.fetchall())
             ch.basic_ack(delivery_tag=method.delivery_tag)
 
-
-
         self.register_receive_message("battery-management-service", handle_bms_input)
-        
+
         for queue_name in self.read_queues:
             self.read_queues[queue_name].start_consuming()
+
 
 if __name__ == "__main__":
     mi = Microservice()
