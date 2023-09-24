4d95bd952f69fd562e65e17c7583e2a3a22146aa
MattPChoy <matthewpchoy@gmail.com>
2023-09-25 01:58:42 +1000

    Running into the same issues as before

--- test/9_RMQ_FastAPI/gateway/main.py
+++ test/9_RMQ_FastAPI/gateway/main.py
@@ -1,6 +1,10 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Query
 from pydantic import BaseModel
+import threading
+import aio_pika
 import pika
+import uuid
+import json
 
 app = FastAPI()
 
@@ -16,6 +20,13 @@ channel.queue_declare(queue='battery-in')
 channel.queue_declare(queue='battery-out')
 
 
+def send_heartbeat():
+    connection.process_data_events()
+
+
+t = threading.Thread(target=send_heartbeat)
+
+
 class BatteryMessage(BaseModel):
     message: str
 
@@ -29,13 +40,37 @@ async def send_battery_message(message: BatteryMessage):
     return {"message": "Message sent to battery-in queue"}
 
 
+async def send_request_and_wait_for_response(battery_id, correlation_id):
+    async with aio_pika.connect_robust("amqp://guest:guest@localhost/%2f") as connection:
+        channel = await connection.channel()
+        queue = await channel.declare_queue("battery-in")
+        response_queue = await channel.declare_queue("battery-out")
+
+        request_message = {
+            "correlation_id": correlation_id,
+            "battery_id": battery_id
+        }
+        await channel.default_exchange.publish(
+            aio_pika.Message(
+                body=json.dumps(request_message).encode('utf-8'),
+                correlation_id=correlation_id,
+                reply_to=response_queue.name
+            ),
+            routing_key=queue.name
+        )
+
+        async with response_queue.iterator() as queue_iter:
+            async for message in queue_iter:
+                if message.correlation_id == correlation_id:
+                    response_message = message.body.decode('utf-8')
+                    return {"message": response_message}
+
+
 @app.get("/getBattery")
-async def get_battery_message():
-    try:
-        method_frame, header_frame, body = channel.basic_get(queue='battery-out')
-        if method_frame:
-            return {"message": body.decode('utf-8')}
-        else:
-            raise HTTPException(status_code=404, detail="No message available in battery-out queue")
-    except Exception as e:
-        raise HTTPException(status_code=500, detail="Failed to retrieve message from battery-out queue")
+async def get_battery_message(battery_id: str = Query(..., description="Battery ID")):
+    correlation_id = str(uuid.uuid4())
+    print(f"Query received with battery_id: {battery_id} and correlation_id: {correlation_id}")
+
+    response = await send_request_and_wait_for_response(battery_id, correlation_id)
+
+    return response
