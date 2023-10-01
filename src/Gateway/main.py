b280e67b40cfc172c8cfc45129ca88cc7cae5ea5
MattPChoy <matthewpchoy@gmail.com>
2023-10-01 11:39:38 +1000

    Trying to get NPM working

--- src/Gateway/main.py
+++ src/Gateway/main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+
 from views import BatteryManagement, UserManagement, Disco
 from Common.BusClient import BusClient
 
@@ -7,6 +9,8 @@ import os
 print(os.listdir(os.getcwd()), end="\n")
 
 app = FastAPI()
+templates = Jinja2Templates(directory="./frontend/build/")
+
 bus_client = BusClient()
 bus_client.send_discovery("Gateway")
 
